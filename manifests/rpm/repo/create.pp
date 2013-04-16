ca7b987d96ae00bf09bc4d84623d2c9815b4838a
Tron Thongsringklee <tron@bashton.com>
2013-04-16 15:39:50 +0100

    Another typo

--- manifests/rpm/repo/create.pp
+++ manifests/rpm/repo/create.pp
@@ -3,7 +3,7 @@ define pulp::rpm::repo::create ($feedurl, $time="01:00:00", $interval="P5D") {
   exec { "pulp-admin-rpm-repo-create-${name}":
     command => "/usr/bin/pulp-admin rpm repo create --repo-id ${name} --feed $feedurl \
                 && /usr/bin/pulp-admin rpm repo sync schedules create --repo-id ${name} -s \"$(date +%Y-%m-%d)T${time}Z/${interval}\" \
-                && /usr/bin/pulp-admin rpm repo sync --repo-id ${name} --bg",
+                && /usr/bin/pulp-admin rpm repo sync run --repo-id ${name} --bg",
     unless  => "/usr/bin/pulp-admin rpm repo update --repo-id ${name} --description \"Last checked by Puppet on $(date)\"",
     require => Package['pulp-admin-client'],
   }
