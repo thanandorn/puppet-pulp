fbd978a162b964c8864e872b8dda49bb70f59526
Tron Thongsringklee <tron@bashton.com>
2013-04-18 10:21:57 +0100

    Add Consumer Group Module

--- manifests/consumer/group.pp
+++ manifests/consumer/group.pp
@@ -1,21 +1,24 @@
 define pulp::consumer::group (
   $type    = 'rpm',
-  $ensure  = 'present',
-  $groupid = $name) {
+  $ensure  = present,
+  $groupid = $name
+) {
 
   case $ensure {
     default: { err("unknown ensure value ${ensure}") }
     present: {
       exec { "pulp-consumergroup-create-${groupid}":
-        command => "/usr/bin/pulp-admin $type consumer group create --group-id ${groupid}",
-        unless  => "test \"$(/usr/bin/pulp-admin $type consumer group list | grep Id | grep -o ${groupid} )\" = \"${groupid}\" ",
+        command => "pulp-admin $type consumer group create --group-id ${groupid}",
+        unless  => "test \"$(pulp-admin $type consumer group list | grep Id | grep -o ${groupid} )\" = \"${groupid}\" ",
+        path    => ['/usr/bin', '/bin'],
         require => Package['pulp-admin-client'],
       }
     }
     absent: {
       exec { "pulp-consumergroup-delete-${groupid}":
-        command => "/usr/bin/pulp-admin $type consumer group delete --group-id ${groupid}",
-        onlyif  => "test \"$(/usr/bin/pulp-admin $type consumer group list | grep Id | grep -o ${groupid} )\" = \"${groupid}\" ",
+        command => "pulp-admin $type consumer group delete --group-id ${groupid}",
+        onlyif  => "test \"$(pulp-admin $type consumer group list | grep Id | grep -o ${groupid} )\" = \"${groupid}\" ",
+        path    => ['/usr/bin', '/bin'],
         require => Package['pulp-admin-client'],
       }
     }
--- manifests/repo.pp
+++ manifests/repo.pp
@@ -4,7 +4,8 @@ define pulp::repo (
   $mirror   = true,
   $type     = 'rpm',
   $time     = '01:00:00',
-  $interval = 'P5D') {
+  $interval = 'P5D'
+) {
 
   case $ensure {
     default: { err("unknown ensure value ${ensure}") }
@@ -13,17 +14,19 @@ define pulp::repo (
         default: { err("unknown mirror value ${mirror}") }
         true: {
           exec { "pulp-${type}-repo-create-${name}":
-            command => "/usr/bin/pulp-admin ${type} repo create --repo-id ${name} --feed $feedurl \
-                        && /usr/bin/pulp-admin ${type} repo sync schedules create --repo-id ${name} -s \"$(date +%Y-%m-%d)T${time}Z/${interval}\" \
-                        && /usr/bin/pulp-admin ${type} repo sync run --repo-id ${name} --bg",
-            unless  => "/usr/bin/pulp-admin ${type} repo update --repo-id ${name} --description \"Last checked by Puppet on $(date)\"",
+            command => "pulp-admin ${type} repo create --repo-id ${name} --feed $feedurl \
+                        && pulp-admin ${type} repo sync schedules create --repo-id ${name} -s \"$(date +%Y-%m-%d)T${time}Z/${interval}\" \
+                        && pulp-admin ${type} repo sync run --repo-id ${name} --bg",
+            unless  => "pulp-admin ${type} repo update --repo-id ${name} --description \"Last checked by Puppet on $(date)\"",
+            path    => ['/usr/bin', '/bin'],
             require => Package['pulp-admin-client'],
           }
         }
         false: {
           exec { "pulp-${type}-repo-create-${name}":
-            command => "/usr/bin/pulp-admin ${type} repo create --repo-id ${name}",
-            unless  => "/usr/bin/pulp-admin ${type} repo update --repo-id ${name} --description \"Last checked by Puppet on $(date)\"",
+            command => "pulp-admin ${type} repo create --repo-id ${name}",
+            unless  => "pulp-admin ${type} repo update --repo-id ${name} --description \"Last checked by Puppet on $(date)\"",
+            path    => ['/usr/bin', '/bin'],
             require => Package['pulp-admin-client'],
           }
         }
@@ -31,8 +34,9 @@ define pulp::repo (
     }
     absent: {
       exec { "pulp-${type}-repo-delete-${name}":
-        command => "/usr/bin/pulp-admin ${type} repo delete --repo-id ${name}",
-        onlyif  => "/usr/bin/pulp-admin ${type} repo update --repo-id ${name} --description \"Last checked by Puppet on $(date)\"",
+        command => "pulp-admin ${type} repo delete --repo-id ${name}",
+        onlyif  => "pulp-admin ${type} repo update --repo-id ${name} --description \"Last checked by Puppet on $(date)\"",
+        path    => ['/usr/bin', '/bin'],
         require => Package['pulp-admin-client'],
       }
     }
