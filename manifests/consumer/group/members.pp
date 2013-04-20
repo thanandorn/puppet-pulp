042749823c0827012a7bd649524e81930e98999f
Tron Thongsringklee <tron@bashton.com>
2013-04-20 15:12:36 +0100

    Add Group Member Module

---
+++ manifests/consumer/group/members.pp
@@ -0,0 +1,31 @@
+define pulp::consumer::group::members (
+  $type       = 'rpm',
+  $ensure     = present,
+  $groupid,
+  $consumerid
+) {
+
+  case $ensure {
+    default: { err("unknown ensure value ${ensure}") }
+    present: {
+      exec { "pulp-consumergroup-${groupid}-add-${consumerid}" :
+        command => "pulp-admin $type consumer group members add \
+                    --group-id ${groupid} --consumer-id ${consumerid}",
+        unless  => "test \"$(pulp-admin $type consumer group list --group-id ${groupid} \
+                    | grep \"Id.*${consumerid}\" | grep -o ${consumerid} )\" = \"${consumerid}\" ",
+        path    => ['/usr/bin', '/bin'],
+        require => Package['pulp-admin'],
+      }
+    }
+    absent: {
+      exec { "pulp-consumergroup-${groupid}-del-${consumerid}" :
+        command => "pulp-admin $type consumer group members remove \
+                    --group-id ${groupid} --consumer-id ${consumerid}",
+        onlyif  => "test \"$(pulp-admin $type consumer group list --group-id ${groupid} \
+                    | grep \"Id.*${consumerid}\" | grep -o ${consumerid} )\" = \"${consumerid}\" ",
+        path    => ['/usr/bin', '/bin'],
+        require => Package['pulp-admin'],
+      }
+    }
+  }
+}
