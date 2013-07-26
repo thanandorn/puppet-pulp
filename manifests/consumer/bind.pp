979fd7aa5f6fc3e1b788a042d0ee2389bdd1c957
Tron Thongsringklee <tron@bashton.com>
2013-07-26 14:48:47 +0100

    Add pulp-consumer bind/unbind

---
+++ manifests/consumer/bind.pp
@@ -0,0 +1,30 @@
+#
+# Only available for RPM repositories
+#
+define pulp::consumer::bind (
+  $type   = 'rpm',
+  $ensure = present,
+  $repoid = $name
+) {
+
+  case $ensure {
+    default: { err("unknown ensure value ${ensure}") }
+    present: {
+      exec { "pulp-consumer-bind-${repoid}":
+        command => "pulp-consumer $type bind --repo-id ${repoid}",
+        unless  => "/bin/grep \"name.*=.*${repoid}\" /etc/yum.repos.d/pulp.repo",
+        path    => ['/usr/bin', '/bin'],
+        require => Package['pulp-consumer-client'],
+      }
+    }
+    absent: {
+      exec { "pulp-consumer-unbind-${repoid}":
+        command => "pulp-admin $type unbind --repo-id ${repoid} --force",
+        onlyif  => "/bin/grep \"name.*=.*${repoid}\" /etc/yum.repos.d/pulp.repo",
+        path    => ['/usr/bin', '/bin'],
+        require => Package['pulp-consumer-client'],
+      }
+    }
+  }
+}
+
