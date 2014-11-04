9a0b0767c0089506194b1b614cb2a82db8eb805e
wingZero21 <devestator21@gmail.com>
2014-11-04 12:33:36 +0000

    Amend yum repo to have name required field
    Amend yum repo to have name required field

--- manifests/yum.pp
+++ manifests/yum.pp
@@ -1,12 +1,14 @@
 class pulp::yum ($major_release = '6') {
 
   yumrepo { 'pulp-init':
+    name     => "pulp-init",
     baseurl  => "http://repos.fedorapeople.org/repos/pulp/pulp/stable/latest/${major_release}Server/${architecture}/",
     enabled  => 1,
     gpgcheck => 0,
   }
 
   yumrepo { 'pulp-epel':
+    name     => "pulp-epel",
     baseurl  => "http://mirrors.coreix.net/fedora-epel/${major_release}/${architecture}/",
     enabled  => 1,
     gpgcheck => 0,
